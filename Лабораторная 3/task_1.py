52f303d685991d908b8f62aae663fb875fc2ed2a
doomshak <burdyugov_999@mail.ru>
2025-01-31 18:16:29 +0300

    Add files via upload

---
+++ Лабораторная 3/task_1.py
@@ -0,0 +1,62 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self):
+        """Свойство для получения названия книги. Изменение запрещено."""
+        return self._name
+
+    @property
+    def author(self):
+        """Свойство для получения автора книги. Изменение запрещено."""
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        """Свойство для установки количества страниц с проверкой."""
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
+        self._pages = value
+
+    def __str__(self):
+        """Перегружаем метод __str__ для добавления информации о страницах."""
+        return f"Бумажная книга {self.name}. Автор {self.author}. Количество страниц: {self.pages}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        """Свойство для установки продолжительности с проверкой."""
+        if not isinstance(value, (int, float)) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом.")
+        self._duration = value
+
+    def __str__(self):
+        return f"Аудиокнига {self.name}. Автор {self.author}. Продолжительность: {self.duration:.2f} часов"
